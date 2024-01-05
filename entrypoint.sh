b1a4cb07557d39356826117d5a09d255470155f6
ArtemBelz <41280642+ArtemBelz@users.noreply.github.com>
2024-01-05 18:32:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -Eeuxo pipefail
 cd $GITHUB_WORKSPACE
-rustup default stable
+rustup default 1.74.0-x86_64-unknown-linux-musl
 rustup target add x86_64-unknown-linux-musl
 bash -c "$*"
